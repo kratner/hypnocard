7b26812d6b1f853170e1a1a3df839f835ca8738d
Keith Ratner <keith.ratner@gmail.com>
2023-08-10 08:36:37 -0700

    Tweak animation

--- src/components/HypnoCard.tsx
+++ src/components/HypnoCard.tsx
@@ -1,13 +1,18 @@
-import React, { FC } from 'react';
+import React, { FC } from "react";
 
 interface HypnoCardProps {
-  name: string;
-  title: string;
-  phone: string;
-  email: string;
+  name?: string;
+  title?: string;
+  phone?: string;
+  email?: string;
 }
 
-const HypnoCard: FC<HypnoCardProps> = ({ name, title, phone, email }) => {
+const HypnoCard: FC<HypnoCardProps> = ({
+  name = "John Smith",
+  title = "Title",
+  phone = "123-456-7890",
+  email = "john.smith@example.com",
+}) => {
   return (
     <div className="hypno-card">
       <div className="hypno-card-header">
@@ -15,10 +20,8 @@ const HypnoCard: FC<HypnoCardProps> = ({ name, title, phone, email }) => {
         <p className="hypno-card-title">{title}</p>
       </div>
       <div className="hypno-card-content">
-        <p className="hypno-card-phone">{phone}</p>        
-        <a href={`mailto:${email}`} className="hypno-card-email">
-          {email}
-        </a>
+        <p className="hypno-card-phone">{phone}</p>
+        <p className="hypno-card-email">{email}</p>
       </div>
     </div>
   );
